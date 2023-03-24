7da3502c6f8af275ee6e4bb801fe17de5584dbcd
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-03-23 23:02:22 -0400

    eliminated one error

--- script.js
+++ script.js
@@ -3,6 +3,30 @@
 // Function to calculate the operations use "getelementbyID?"
 
 const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculator__keys')
+const keys = document.querySelector('.calculator__keys')
 
 //We can add an event listener to all the keys this way
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')){
+    //This will create an action
+    const key = e.target
+    const action = key.dataset.action
+  if(
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide' 
+) {
+  console.log('operator key!')
+}
+  
+  
+  }
+})
+
+if (!action) {
+  console.log('number key!')
+
+
+}
+
