be6018d4ca6ba5df59fbcb83e81f2d7f59480c71
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-03-23 21:01:06 -0400

    building JS functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+//What will we need for the calculator to work...
+// Event listeners for the buttons
+// Function to calculate the operations use "getelementbyID?"
+
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+//We can add an event listener to all the keys this way
