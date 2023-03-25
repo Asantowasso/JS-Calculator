9857c67aa86bcdaf85d280fde0932cfcde24a1ea
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-03-25 14:40:39 -0400

    able to see numbers and characters in the display field

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 // Function to calculate the operations use "getelementbyID?"
 
 //We will need functions to clear the screen, display inputs and solve what the user enters
+
+function display(val){
+  document.getElementById('result').value += val
+
+  return val
+}
