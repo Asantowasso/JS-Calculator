ef3619a13d071d9a74c8c9563ca119945878cc1e
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-03-25 16:14:02 -0400

    able to solve inputs

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function display(val){
 
   return val
 }
+
+function solve(){
+  let x = document.getElementById('result').value
+
+  let y = eval(x);
+
+  document.getElementById('result').value = y
+
+  return y
+}
